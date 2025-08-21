ad2b92a46d7983b16fcf5d329f4058578f56d252
ahyeonbaek <4641090@naver.com>
2025-08-21 20:26:13 +0900

    feat: 유저 정보 상태관리 기능 추가
    -updateUser, logout, addReservation, updateReservation,deleteReservation 메서드 추가,
    -persist 적용

---
+++ src/zustand/userStore.ts
@@ -0,0 +1,69 @@
+import { create } from "zustand";
+import { Reservation, User } from "../types/type";
+import { persist } from "zustand/middleware";
+
+interface userState {
+  user: User | null;
+  isLoggedIn: boolean;
+
+  setUser: (user: User) => void;
+  updateUser: (updates: Partial<Omit<User, "id">> & { id: string }) => void;
+  logout: () => void;
+
+  addReservation: (reservation: Reservation) => void;
+  updateReservation: (
+    updates: Partial<Omit<Reservation, "id">> & { id: string }
+  ) => void;
+  deleteReservation: (reservation: Reservation) => void;
+}
+
+export const useUserStore = create<userState>()(
+  persist(
+    (set) => ({
+      user: null,
+      isLoggedIn: false,
+
+      setUser: (user) => set({ user, isLoggedIn: true }),
+      updateUser: (updates) =>
+        set((state) => ({
+          user: state.user ? { ...state.user, ...updates } : null,
+        })),
+      logout: () => set({ user: null, isLoggedIn: false }),
+
+      addReservation: (reservation) =>
+        set((state) => ({
+          user: state.user
+            ? {
+                ...state.user,
+                reservations: [...(state.user.reservations || []), reservation],
+              }
+            : null,
+        })),
+      updateReservation: (updates) =>
+        set((state) => ({
+          user: state.user
+            ? {
+                ...state.user,
+                reservations: state.user.reservations?.map((r) =>
+                  r.id === updates.id ? { ...r, ...updates } : r
+                ),
+              }
+            : null,
+        })),
+      deleteReservation: (reservation) =>
+        set((state) => ({
+          user: state.user
+            ? {
+                ...state.user,
+                reservations: state.user.reservations?.filter(
+                  (r) => r.id !== reservation.id
+                ),
+              }
+            : null,
+        })),
+    }),
+    { name: "userStorage" }
+  )
+);
+
+export default useUserStore;
