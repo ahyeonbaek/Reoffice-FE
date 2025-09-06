67e67ee07c1594d80e2be2aca94dbdce5348f6a5
ahyeonbaek <4641090@naver.com>
2025-09-06 15:34:57 +0900

    feat: 실제 유저의 정보 표시, logout 추가

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,12 +1,14 @@
+import useUserStore from "@/zustand/userStore";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const navigate = useNavigate();
   const [profileImg, setProfileImg] = useState("/img/Vector.png");
+  const { logout, user } = useUserStore();
 
   return (
-    <div className="w-[250px] h-[500px] bg-[#f5f5f5] p-[20px] rounded-lg">
+    <div className="w-[250px] h-[500px] bg-blue-100 p-[20px] rounded-lg shadow-lg">
       <div className="flex justify-end">
         <img
           src="/img/Bell.png"
@@ -17,14 +19,14 @@ const Sidebar = () => {
       <div className="pt-[20px] pb-[20px] border-b">
         <div className="flex justify-center pt-[20px] pb-[20px]">
           <img
-            src={profileImg}
+            src={user?.profileImg ? user.profileImg : profileImg}
             alt="프로필 이미지"
             className="w-[100px] h-[100px]"
           />
         </div>
         <div className="flex jusify-center items-center gap-[10px] flex-col">
-          <p>이름</p>
-          <p>이메일</p>
+          <p>{user?.name}</p>
+          <p>{user?.email}</p>
         </div>
         <div className="flex justify-end ">
           <img
@@ -35,11 +37,16 @@ const Sidebar = () => {
           />
         </div>
       </div>
-      <ul className="pt-[20px] pb-[20px]">
-        <li>
-          <a href="">회의실 예약 목록</a>
-        </li>
-      </ul>
+      <div>
+        <ul className="pt-[20px] pb-[20px]">
+          <li>
+            <a href="/list">회의실 예약 목록</a>
+          </li>
+        </ul>
+        <div>
+          <button onClick={logout}>logout</button>
+        </div>
+      </div>
     </div>
   );
 };
