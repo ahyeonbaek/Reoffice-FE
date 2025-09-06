ddef6b43342f27e775148dc57ca8ef4905dc98ed
ahyeonbaek <4641090@naver.com>
2025-09-06 16:11:46 +0900

    feat: alias 경로 별칭 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import removeConsole from "vite-plugin-remove-console";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,18 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@apis": path.resolve(__dirname, "src/apis"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@consts": path.resolve(__dirname, "src/consts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@route": path.resolve(__dirname, "src/route"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@zustand": path.resolve(__dirname, "src/zustand"),
     },
   },
 });
