c6a3ffc1f6dae808e4a19832c2d9874f2785383e
ahyeonbaek <4641090@naver.com>
2025-09-06 16:27:46 +0900

    feat: import 경로 수정, BaseLayout에서 LoginPage 컴포넌트 분리, 유저 인증이 필요한 컴포넌트 ProtectRoute 추가

--- src/route/routes.tsx
+++ src/route/routes.tsx
@@ -1,35 +1,52 @@
+import LoginPage from "@/pages/Login";
 import { createBrowserRouter } from "react-router-dom";
-import MainPage from "../pages/Main";
 import BaseLayout from "./BaseLayout";
-import ListPage from "../pages/ReservationList";
-import ReservationEditPage from "../pages/ReservationEdit";
-import ProfilePage from "../pages/Profile";
-import LoginPage from "../pages/Login/index";
+import MainPage from "@/pages/Main";
+import ListPage from "@/pages/ReservationList";
+import ReservationEditPage from "@/pages/ReservationEdit";
+import ProfilePage from "@/pages/Profile";
+import ProtectRoute from "./ProtectRoute";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
   {
     path: "/",
     element: <BaseLayout />,
     children: [
       {
-        path: "/login",
-        element: <LoginPage />,
-      },
-      {
         path: "/main",
-        element: <MainPage />,
+        element: (
+          <ProtectRoute>
+            <MainPage />
+          </ProtectRoute>
+        ),
       },
       {
         path: "/list",
-        element: <ListPage />,
+        element: (
+          <ProtectRoute>
+            <ListPage />
+          </ProtectRoute>
+        ),
       },
       {
         path: "/reservation/:id",
-        element: <ReservationEditPage />,
+        element: (
+          <ProtectRoute>
+            <ReservationEditPage />
+          </ProtectRoute>
+        ),
       },
       {
         path: "/profile",
-        element: <ProfilePage />,
+        element: (
+          <ProtectRoute>
+            <ProfilePage />
+          </ProtectRoute>
+        ),
       },
     ],
   },
